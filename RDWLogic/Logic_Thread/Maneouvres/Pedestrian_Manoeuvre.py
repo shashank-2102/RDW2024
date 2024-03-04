5841f596d558f433c6da69d4ba91631e4d4b92d4
Valkyrri025 <zzy76681@gmail.com>
2024-03-04 16:39:20 +0100

    Update Pedestrian_Manoeuvre.py

--- RDWLogic/Logic_Thread/Maneouvres/Pedestrian_Manoeuvre.py
+++ RDWLogic/Logic_Thread/Maneouvres/Pedestrian_Manoeuvre.py
@@ -34,30 +34,30 @@ class Pedestrian_Maneouvre:
         if y3<0.6 and y3>0.2 and x3>0.5:  #relative positions for now
             self.pedestrian_crossing_detected = True
     
-    def pedestrian_manoeuvre(self):
+    def pedestrian_logic(self):
     
         if not self.pedestrian_detected:
             if is_moving():
                 continue
             else:
-                set_velocity(self, get_velocity(self))   #no pedestrian found, keep moving or start moving
+                self._TSpeed =  get_velocity(self)   #no pedestrian found, keep moving or start moving
         
         elif self.pedestrian_sign_detected:   
             if is_moving() and y1<0.4:    #arbitrary value
-                stop()
+                self._TSpeed = 0
             elif not is_moving():
                 continue    #pedestrian found and pedestrian sign found, stop if the car is within a distance
             
         elif self.pedestrian_crossing_detected:
             if is_moving() and y3<0.4:    #arbitrary value
-                stop()
+                self._TSpeed = 0
             elif not is_moving():
                 continue    #pedestrian found and pedestrian crossing found, stop if the car is within a distance
         
         else:
             print('no pedestrian crossing or pedestrian sign detected')
             continue
-        return
+        return self._TSpeed, self._distance
             
             
                 
