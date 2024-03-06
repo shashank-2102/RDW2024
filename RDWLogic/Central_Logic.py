06c7e89723c4c5381a183ed045fe266e4d59fb6d
shashank-2102 <shashank.prabhu21@gmail.com>
2024-03-06 13:21:42 +0100

    Fixed func1

--- RDWLogic/Central_Logic.py
+++ RDWLogic/Central_Logic.py
@@ -1,18 +1,17 @@
 def Func1(objectList):
-    # Funct1 get constantly recieves all objects
     tSpeed = 0
-    lDistance = 0
+    lDistance = float('inf') 
     for obj in objectList:
-        if obj.getDistance() > lDistance:
+        if obj.getDistance() < lDistance:
             lDistance = obj.getDistance()
             tSpeed = obj.getTSpeed()
 
+    return [tSpeed, lDistance]
 
 
-
-
-    return tSpeed, lDistance
 
 def Final_Function():
     pass
+
+
 
