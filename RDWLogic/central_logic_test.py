d38b4c68b15910e27f8f9a730b8e78ec434360c6
shashank-2102 <shashank.prabhu21@gmail.com>
2024-03-06 13:04:59 +0100

    Split test

--- RDWLogic/central_logic_test.py
+++ RDWLogic/central_logic_test.py
@@ -23,16 +23,18 @@ obstacle3 = Test_Obstacle(35, 40)
 
 
 class func1_test(unittest.TestCase):
-    def test_Func1(self):
-        # Test case 1: Empty object list
+    def test_Func1_empty(self):
+        # Empty object list
         objectList = []
         self.assertEqual(Func1(objectList), [0, 0])
-        
-        # Test case 2: Object list with one object
+
+    def test_Func1_one(self):
+        # Object list with one object
         objectList = [obstacle1]
         self.assertEqual(Func1(objectList), [50, 10])
 
-        # Test case 3: Object list with multiple objects
+    def test_Func1_multi(self):
+        # Object list with multiple objects
         objectList = [obstacle1, obstacle2, obstacle3]
         self.assertEqual(Func1(objectList), [50, 10])
 
