36ef467ffe50ec91bfd13ec6665d9c07168a64c5
VJandl <jandlvojtech@outlook.com>
2024-03-17 19:10:50 +0100

    Speed limit test implemented

--- RDWLogic/Logic_Thread/Maneouvres/Speed_Limit.py
+++ RDWLogic/Logic_Thread/Maneouvres/Speed_Limit.py
@@ -3,10 +3,16 @@ class Speed_Limit:
     def __init__(self):
         self.tSpeed = 0
         self.distance = 0
-        self.speed_limit_detected = True
+        self.speed_limit_detected = False
+    
+    def getDistance(self):
+        return self._distance
+    
+    def getTSpeed(self):
+        return self._TSpeed
         
     # assuming objects by type has all variables  
-    def speed_sign(self, objects_by_type, tSpeed):
+    def speed_sign(self, objects_by_type, tSpeed, Distance):
         
         
         #if '10' in objects_by_type_front  && objects_by_type_front['10'][0][3] > 400:
@@ -15,12 +21,12 @@ class Speed_Limit:
             self.speed_limit_detected = True
             self.distance = 0
         #if '15' in objects_by_type_front  && objects_by_type_front['15'][0][3] > 400:
-        elif objects_by_type['15']:
+        elif objects_by_type['15'] and self.speed_limit_detected == False:
             self.tSpeed = 15
             self.speed_limit_detected = True
             self.distance = 0
         #if '20' in objects_by_type_front  && objects_by_type_front['20'][0][3] > 400:
-        elif objects_by_type['20']:
+        elif objects_by_type['20'] and self.speed_limit_detected == False:
             self.tSpeed = 20
             self.speed_limit_detected = True
             self.distance = 0
@@ -28,17 +34,5 @@ class Speed_Limit:
             self.speed_limit_detected = False
             self.tSpeed = tSpeed
             # if -1 it is not detected
-            self.distance = -1
+            self.distance = Distance
         return (self.speed_limit_detected, self.tSpeed, self.distance)
-#test
-tSpeed = 33
-Distance = -1
-Speed_limit_detected = False
-data = {
-    '10':[],
-    '15':[],
-    '20':[]
-}
-speed_limit_instance = Speed_Limit()
-Speed_limit_detected, tSpeed, Distance = speed_limit_instance.speed_sign(data,tSpeed)
-print(Speed_limit_detected, tSpeed, Distance)
