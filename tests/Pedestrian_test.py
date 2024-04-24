95517a7ccfc4980af452c51f9153977aa84be5d8
Valkyrri025 <zzy76681@gmail.com>
2024-04-24 22:41:56 +0200

    Update Pedestrian_test.py

--- tests/Pedestrian_test.py
+++ tests/Pedestrian_test.py
@@ -12,27 +12,11 @@ class PD_Logic_Test(unittest.TestCase):
         coordinates1 = [0.4, 0.5, 0.7, 0.6]
         
         # Call the PD_check method and get the result
-        result1 = Pedestrian_instance.pedestrian_check(coordinates1)
+        result1 = Pedestrian_instance.person_logic(coordinates1)[0]
         result2 = True
-        result3 = True   # When pedestrian isn't found, it doesn't matter if there is pedestrian sign or crossing
         
         # Assert the expected behavior
-        self.assertEqual(Pedestrian_instance.pedestrian_logic(result1, result2, result3), [50, 0]) #set correct value
-
-    def test_PD_Logic_PD_and_PD_sign(self):
-        # Instantiate Pedestrian
-        Pedestrian_instance = Pedestrian_Maneouvre()
-        
-        # Input parameters
-        coordinates1 = [0.6, 0.5, 0.7, 0.6] # distance for pedestrian
-        coordinates2 = [0.6, 0.5, 0.7, 0.6] # distance for pedestrian sign
-        
-        # Call the PD_check method and get the result
-        result1 = Pedestrian_instance.pedestrian_check(coordinates1)
-        result2 = Pedestrian_instance.pedestrian_sign_check(coordinates2)
-        result3 = True   # When pedestrian and sign are found, set speed 0
-        
-        self.assertEqual(Pedestrian_instance.pedestrian_logic(result1, result2, result3), [0, 0]) #set correct value
+        self.assertEqual(Pedestrian_instance.pedestrian_logic(result1, result2), 50) #set correct value
     
     def test_PD_Logic_PD_and_PD_crossing(self):
         # Instantiate Pedestrian
@@ -43,11 +27,10 @@ class PD_Logic_Test(unittest.TestCase):
         coordinates3 = [0.6, 0.5, 0.7, 0.6] # distance for pedestrian crossing
         
         # Call the PD_check method and get the result
-        result1 = Pedestrian_instance.pedestrian_check(coordinates1)
-        result2 = True
-        result3 = Pedestrian_instance.pedestrian_crossing_check(coordinates3)
+        result1 = Pedestrian_instance.person_logic(coordinates1)[0]
+        result2 = Pedestrian_instance.crossing_logic(coordinates3)[0]
         
-        self.assertEqual(Pedestrian_instance.pedestrian_logic(result1, result2, result3), [0, 0]) #set correct value
+        self.assertEqual(Pedestrian_instance.pedestrian_logic(result1, result2), 0) #set correct value
         
 if __name__ == '__main__':
     unittest.main()
